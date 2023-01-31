7909699ea11485180e585dded4abb7c82703eeeb
Azil Espinosa <azil.espinosa.ii@accenture.com>
2023-01-31 10:17:56 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zbtp/day6exercise3espinosa/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zbtp/day6exercise3_espinosa/test/unit/controller/Overview.controller"
+], function () {
+	"use strict";
+});
